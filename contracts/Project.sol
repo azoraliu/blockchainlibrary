fb2fbdd9e59daeb14aea2f2208ed4fd8d3c26a0d
mercuryZ <15210820708@163.com>
2020-04-13 15:55:11 +0800

    project contract

---
+++ contracts/Project.sol
@@ -0,0 +1,120 @@
+pragma solidity ^0.4.17;
+
+library SafeMath {
+    function mul(uint a, uint b) internal pure returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint a, uint b) internal pure returns (uint) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint a, uint b) internal pure returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint a, uint b) internal pure returns (uint) {
+        uint c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+contract ProjectList {
+    using SafeMath for uint;
+    address[] public projects;
+
+    function createProject(string _description, uint _minInvest, uint _maxInvest, uint _goal) public {
+        address newProject = new Project(_description, _minInvest, _maxInvest, _goal, msg.sender);
+        projects.push(newProject);
+    }
+
+}
+
+contract Project {
+    struct Payment {
+        string description;
+        uint amount;
+        address receiver;
+        bool completed;
+        address[] voters;
+    }
+
+    address public owner;
+    string public description;
+    uint public minInvest;
+    uint public maxInvest;
+    uint public goal;
+    address[] public investors;
+    Payment[] public payments;
+
+    constructor(string _description, uint _minInvest, uint _maxInvest, uint _goal, address _owner) public {
+        owner = msg.sender;
+        description = _description;
+        minInvest = _minInvest;
+        maxInvest = _maxInvest;
+        goal = _goal;
+    }
+
+    function contribute() public payable {
+        require(msg.value >= minInvest);
+        require(msg.value <= maxInvest);
+        require(address(this).balance <= goal);
+
+        investors.push(msg.sender);
+    }
+
+    function createPayment(string _description, uint _amount, address _receiver) public {
+        Payment memory newPayment = Payment({
+            description: _description,
+            amount: _amount,
+            receiver: _receiver,
+            completed: false,
+            voters: new address[](0)
+        });
+
+        payments.push(newPayment);
+    }
+
+    function approvePayment(uint index) public {
+        Payment storage payment = payments[index];
+
+        // must be investor to vote
+        bool isInvestor = false;
+        for (uint i = 0; i < investors.length; i++) {
+            isInvestor = investors[i] == msg.sender;
+            if (isInvestor) {
+                break;
+            }
+        }
+        require(isInvestor);
+
+        // can not vote twice
+        bool hasVoted = false;
+        for (uint j = 0; j < payment.voters.length; j++) {
+            hasVoted = payment.voters[j] == msg.sender;
+            if (hasVoted) {
+                break;
+            }
+        }
+        require(!hasVoted);
+
+        payment.voters.push(msg.sender);
+    }
+
+    function doPayment(uint index) public {
+        Payment storage payment = payments[index];
+
+        require(!payment.completed);
+        require(payment.voters.length > (investors.length / 2));
+
+        payment.receiver.transfer(payment.amount);
+        payment.completed = true;
+    }
+}
